5bc0b2671343e25e6f50685d91102128865b4fbb
merkesphere <mark@esphere.com>
2025-09-22 10:57:43 -0700

    chore: update package version to 1.2.0 and dependencies
    - Bump version from 0.0.2 to 1.2.0 in package.json and package-lock.json
    - Update react-scripts version to 5.0.1
    - Upgrade cross-env to version 10.0.0
    - Remove deprecated dependencies related to jsonjoy.com
    - Update webpack-dev-server to version 4.15.1
    - Add monkey patching for webpack-dev-server to handle deprecated options in start-clean.js
    - Suppress specific webpack dev server deprecation warnings

--- scripts/start-clean.js
+++ scripts/start-clean.js
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
 
+// Monkey patch the webpack-dev-server to handle deprecated options
+const Module = require('module');
+const originalRequire = Module.prototype.require;
+
+Module.prototype.require = function (id) {
+  const module = originalRequire.apply(this, arguments);
+  
+  // Patch webpack-dev-server configuration schema
+  if (id === 'webpack-dev-server' && module.schema) {
+    // Add deprecated properties back to schema to prevent errors
+    if (module.schema.properties && !module.schema.properties.onAfterSetupMiddleware) {
+      module.schema.properties.onAfterSetupMiddleware = { type: 'function' };
+      module.schema.properties.onBeforeSetupMiddleware = { type: 'function' };
+    }
+  }
+  
+  return module;
+};
+
 // Suppress specific webpack dev server deprecation warnings
 const originalEmit = process.emit;
 process.emit = function (name, data, ...args) {
